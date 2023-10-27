7f4d28f51793fcb56ddebbfc19a5b57575930c84
Andrew Slack <me@andyslack.com>
2023-10-27 16:26:13 +0200

    Adding codesandbox

--- backend/pnpm-link.sh
+++ backend/pnpm-link.sh
@@ -1,9 +1,6 @@
-pnpm link --global @juicyllama/cli
-
-## awaiting fix: https://github.com/juicyllama/framework/issues/35
-
 ## LINK PACKAGES ##
-#pnpm link --global @juicyllama/dev
-#pnpm link --global @juicyllama/core
-#pnpm link --global @juicyllama/utils
-#pnpm link --global @juicyllama/nestjs-redoc
+pnpm link --global @juicyllama/cli
+pnpm link --global @juicyllama/dev
+pnpm link --global @juicyllama/core
+pnpm link --global @juicyllama/utils
+pnpm link --global @juicyllama/nestjs-redoc
---
+++ backend/pnpm-unlink.sh
@@ -0,0 +1,6 @@
+## UNLINK PACKAGES ##
+pnpm unlink --global @juicyllama/cli
+pnpm unlink --global @juicyllama/dev
+pnpm unlink --global @juicyllama/core
+pnpm unlink --global @juicyllama/utils
+pnpm unlink --global @juicyllama/nestjs-redoc
