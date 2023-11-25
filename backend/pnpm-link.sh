ba70641914445cdf0cd7552afd6c4c97e768c323
Yurko Fedoriv <yurko.fedoriv@syndigo.com>
2023-11-25 12:50:17 +0200

    fixes: ModuleRef broken DI juicyllama/framework#69, TypeORM Issue juicyllama/framework#70

--- backend/pnpm-link.sh
+++ backend/pnpm-link.sh
@@ -3,3 +3,7 @@ pnpm link --global @juicyllama/cli
 pnpm link --global @juicyllama/dev
 pnpm link --global @juicyllama/core
 pnpm link --global @juicyllama/utils
+
+# NestJS
+pnpm link --global @nestjs/core
+pnpm link --global @nestjs/typeorm
